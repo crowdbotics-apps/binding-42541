2f65eb1f4e0fefa9a132ef68915531c5d628d579
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-07-31 23:11:28 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "binding-42541",
-  url: "https://binding-42541.botics.co",
-  api: "https://binding-42541.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "binding-42541",
+  "url": "https://binding-42541.botics.co",
+  "api": "https://binding-42541.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
