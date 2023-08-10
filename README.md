050673b0560971367130f980856d614c4594f417
Crowdbotics <team@crowdbotics.com>
2023-08-10 11:24:31 +0000

    add component WalletConnect

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ This section will show any installed modules you add from the Storyboard Modules
 - [react-native-onboarding](modules/onboarding)
 - [react-native-photo-editing](modules/photo-editing)
 - [react-native-splash](modules/splash)
+- [react-native-wallet-connect](modules/wallet-connect)
 
 # Getting started: Frontend
 
--- modules/wallet-connect/components/HistoryList/index.js
+++ modules/wallet-connect/components/HistoryList/index.js
@@ -1,51 +1,24 @@
-import { StyleSheet } from "react-native";
 import React from "react";
 import { View, Text } from "react-native";
 
-const HistoryItem = props => {
-  return <View style={_styles.hQlgUMeA}>
-        <View style={_styles.kgfEognG}>
-            <Text style={_styles.rOzhITfY}>From</Text>
+const HistoryItem = (props) => {
+  return (
+    <View style={{ backgroundColor: "white", borderRadius: 2 }}>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+            <Text style={{ width: "20%" }}>From</Text>
             <Text>{props.title.from}</Text>
         </View>
-        <View style={_styles.zFxuMEDV}>
-            <Text style={_styles.lGDsaYtl}>To:</Text>
-            <Text>{props.title.to}</Text>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+            <Text style={{ width: "20%" }}>To:</Text>
+            <Text >{props.title.to}</Text>
         </View>
-        <View style={_styles.dEiKwdNe}>
-            <Text style={_styles.UzDxmbVI}>Amount</Text>
-            <Text>{props.title.value}</Text>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+            <Text style={{ width: "20%" }}>Amount</Text>
+            <Text >{props.title.value}</Text>
         </View>
 
-    </View>;
+    </View>
+  );
 };
 
 export default HistoryItem;
-
-const _styles = StyleSheet.create({
-  hQlgUMeA: {
-    backgroundColor: "white",
-    borderRadius: 2
-  },
-  kgfEognG: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  rOzhITfY: {
-    width: "20%"
-  },
-  zFxuMEDV: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  lGDsaYtl: {
-    width: "20%"
-  },
-  dEiKwdNe: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  UzDxmbVI: {
-    width: "20%"
-  }
-});
