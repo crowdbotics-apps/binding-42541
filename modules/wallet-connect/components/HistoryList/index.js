92c1d439e56d6f8577dce8b05677def9d12753f8
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-08-10 16:29:14 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/wallet-connect/components/HistoryList/index.js
+++ modules/wallet-connect/components/HistoryList/index.js
@@ -1,24 +1,51 @@
+import { StyleSheet } from "react-native";
 import React from "react";
 import { View, Text } from "react-native";
 
-const HistoryItem = (props) => {
-  return (
-    <View style={{ backgroundColor: "white", borderRadius: 2 }}>
-        <View style={{ display: "flex", flexDirection: "row" }}>
-            <Text style={{ width: "20%" }}>From</Text>
+const HistoryItem = props => {
+  return <View style={_styles.aBydFytO}>
+        <View style={_styles.uTqpgTWw}>
+            <Text style={_styles.fFGcaFfn}>From</Text>
             <Text>{props.title.from}</Text>
         </View>
-        <View style={{ display: "flex", flexDirection: "row" }}>
-            <Text style={{ width: "20%" }}>To:</Text>
-            <Text >{props.title.to}</Text>
+        <View style={_styles.oOXykjLQ}>
+            <Text style={_styles.yNZPyDKT}>To:</Text>
+            <Text>{props.title.to}</Text>
         </View>
-        <View style={{ display: "flex", flexDirection: "row" }}>
-            <Text style={{ width: "20%" }}>Amount</Text>
-            <Text >{props.title.value}</Text>
+        <View style={_styles.IJBSgvfc}>
+            <Text style={_styles.nKgmaxKr}>Amount</Text>
+            <Text>{props.title.value}</Text>
         </View>
 
-    </View>
-  );
+    </View>;
 };
 
 export default HistoryItem;
+
+const _styles = StyleSheet.create({
+  aBydFytO: {
+    backgroundColor: "white",
+    borderRadius: 2
+  },
+  uTqpgTWw: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  fFGcaFfn: {
+    width: "20%"
+  },
+  oOXykjLQ: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  yNZPyDKT: {
+    width: "20%"
+  },
+  IJBSgvfc: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  nKgmaxKr: {
+    width: "20%"
+  }
+});
