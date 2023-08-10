d31e2d4e82bf2a3c6b37b99c5e5340693d0bdbe5
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-08-10 15:59:21 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/bindingAPI/api.js
+++ store/bindingAPI/api.js
@@ -1,77 +1,108 @@
-import axios from "axios"
+import axios from "axios";
 const bindingAPI = axios.create({
   baseURL: "https://binding-42541.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return bindingAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return bindingAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return bindingAPI.post(`/api/v1/login/`, payload.data)
+  return bindingAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_profile_list(payload) {
-  return bindingAPI.get(`/api/v1/profile/`)
+  return bindingAPI.get(`/api/v1/profile/`);
 }
+
 function api_v1_profile_create(payload) {
-  return bindingAPI.post(`/api/v1/profile/`, payload.data)
+  return bindingAPI.post(`/api/v1/profile/`, payload.data);
 }
+
 function api_v1_profile_retrieve(payload) {
-  return bindingAPI.get(`/api/v1/profile/${payload.id}/`)
+  return bindingAPI.get(`/api/v1/profile/${payload.id}/`);
 }
+
 function api_v1_profile_update(payload) {
-  return bindingAPI.put(`/api/v1/profile/${payload.id}/`, payload.data)
+  return bindingAPI.put(`/api/v1/profile/${payload.id}/`, payload.data);
 }
+
 function api_v1_profile_partial_update(payload) {
-  return bindingAPI.patch(`/api/v1/profile/${payload.id}/`, payload.data)
+  return bindingAPI.patch(`/api/v1/profile/${payload.id}/`, payload.data);
 }
+
 function api_v1_profile_destroy(payload) {
-  return bindingAPI.delete(`/api/v1/profile/${payload.id}/`)
+  return bindingAPI.delete(`/api/v1/profile/${payload.id}/`);
 }
+
 function api_v1_signup_create(payload) {
-  return bindingAPI.post(`/api/v1/signup/`, payload.data)
+  return bindingAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function modules_camera_photos_user_list(payload) {
-  return bindingAPI.get(`/modules/camera/photos/user/`)
+  return bindingAPI.get(`/modules/camera/photos/user/`);
 }
+
 function modules_camera_photos_user_retrieve(payload) {
-  return bindingAPI.get(`/modules/camera/photos/user/${payload.id}/`)
+  return bindingAPI.get(`/modules/camera/photos/user/${payload.id}/`);
 }
+
 function modules_camera_upload_image_create(payload) {
-  return bindingAPI.post(`/modules/camera/upload_image/`)
+  return bindingAPI.post(`/modules/camera/upload_image/`);
 }
+
 function rest_auth_login_create(payload) {
-  return bindingAPI.post(`/rest-auth/login/`, payload.data)
+  return bindingAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return bindingAPI.get(`/rest-auth/logout/`)
+  return bindingAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return bindingAPI.post(`/rest-auth/logout/`)
+  return bindingAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return bindingAPI.post(`/rest-auth/password/change/`, payload.data)
+  return bindingAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return bindingAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return bindingAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return bindingAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return bindingAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return bindingAPI.post(`/rest-auth/registration/`, payload.data)
+  return bindingAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return bindingAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return bindingAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return bindingAPI.get(`/rest-auth/user/`)
+  return bindingAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return bindingAPI.put(`/rest-auth/user/`, payload.data)
+  return bindingAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return bindingAPI.patch(`/rest-auth/user/`, payload.data)
+  return bindingAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -96,4 +127,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
